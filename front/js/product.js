154bcc5d150383d13a3ea98fd688c201817907cc
Nicolas Lepers <Lepers.n@gmail.com>
2022-08-25 18:07:54 +0200

    product script

---
+++ front/js/product.js
@@ -0,0 +1,11 @@
+// Recuperation de l'id depuis l'url
+
+let idProduct = 
+console.log(idProduct)//doit correspondre à l'id du produit
+
+// Creation de la variable contentant l'article
+let ... = ...
+
+// MEP du fetch avec Id de l'objet
+
+// MEP de l'affichage et de la boucle
