31ba3a66d6ca611cba0f62aed6fb232e3aad8ce8
Nicolas Lepers <Lepers.n@gmail.com>
2022-08-26 15:37:12 +0200

    victory

--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,31 @@
 // Recuperation de l'id depuis l'url
-
-let idProduct = 
-console.log(idProduct)//doit correspondre à l'id du produit
+let str = window.location //tu regardes dans la barre de nav 
+let url = new URL(str)
+let idProduct = url.searchParams.get('id') //et cherche les paramettres qu tu mets dans une variable
+console.log(idProduct);//doit correspondre à l'id du produit
 
 // Creation de la variable contentant l'article
-let ... = ...
+// let article = 
 
 // MEP du fetch avec Id de l'objet
+let sUrl = " http://localhost:3000/api/products";
+fetch(`${sUrl}/${idProduct}`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let produits = data;
+    console.log(produits);
+    
+    // MEP de l'affichage et de la boucle
+    let img = document.createElement("img"); 
+    let image = document.querySelector('.item__img').appendChild(img);
+    image.setAttribute("src",produits.imageUrl);
+    image.setAttribute("alt",produits.altTxt)
+    console.log(image);
 
-// MEP de l'affichage et de la boucle
+
+
+
+  })
+  
