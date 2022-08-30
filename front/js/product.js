898c68d05443fea05ea891541a5878c666865dc5
Nicolas Lepers <Lepers.n@gmail.com>
2022-08-30 11:01:23 +0200

    step by step

--- front/js/product.js
+++ front/js/product.js
@@ -2,12 +2,8 @@
 let str = window.location //tu regardes dans la barre de nav 
 let url = new URL(str)
 let idProduct = url.searchParams.get('id') //et cherche les paramettres qu tu mets dans une variable
-console.log(idProduct);//doit correspondre à l'id du produit
+//console.log(idProduct);doit correspondre à l'id du produit
 
-// Creation de la variable contentant l'article
-// let article = 
-
-// MEP du fetch avec Id de l'objet
 let sUrl = " http://localhost:3000/api/products";
 fetch(`${sUrl}/${idProduct}`)
   .then(function (response) {
@@ -15,14 +11,13 @@ fetch(`${sUrl}/${idProduct}`)
   })
   .then(function (data) {
     let produits = data;
-    console.log(produits);
+   console.log(data);
     
     // MEP de l'affichage et de la boucle
     let img = document.createElement("img"); 
     let image = document.querySelector('.item__img').appendChild(img);
     image.setAttribute("src",produits.imageUrl);
     image.setAttribute("alt",produits.altTxt)
-    console.log(image);
 
     let title = document.querySelector('#title')
     title.innerHTML = (produits.name)
@@ -31,7 +26,6 @@ fetch(`${sUrl}/${idProduct}`)
     let desc = document.querySelector('#description')
     desc.innerHTML = (produits.description)
  
-
     let defaultSelection = document.createElement("option")
     defaultSelection.innerHTML = ('--SVP, choisissez une couleur --')
     let colors = document.querySelector('#colors')
@@ -39,25 +33,12 @@ fetch(`${sUrl}/${idProduct}`)
 
         for(let i = 0; i<colors.length; i++)
         {
-          let multiColors = document.querySelector('option')
+          let multiColors = document.createElement('option')
           let value = document.querySelector('option')
           value.setAttribute("value", produits.colors[i]);
-          console.log(multiColors);
           multiColors.innerHTML = (produits.colors[i])
-       
-  }})
-  
-
+          console.log(multiColors);
+        }})
       // console.log(defaultSelection);
 
-
-
-
-      /*************Option boucle couleur n*1 */
-       // let multiColors = document.createElement('options')
-        // console.log(multiColors);
-        // let colors = documentquerySelector('#colors')
-        // multiColors.appendChild(colors);
-        // multiColors.setAttribute("value", produits.colors[i]);
-        // multiColors.innerHTML = (produits.colors[i])
-        // color.innerHTML = (produits.colors[i])
+      //document.createElement("option"produits.colors[i])
