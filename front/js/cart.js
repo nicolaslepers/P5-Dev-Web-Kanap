92409374016e929bbec47a223b22c62359fe469c
Nicolas Lepers <Lepers.n@gmail.com>
2022-09-16 11:31:47 +0200

    comment

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,20 +1,17 @@
 import { basketTab } from "./product.js";
 import { apiRecup } from "./script.js";
-// for(let i = 0; i < basketTab.length; i++) {                                    equivaux à basketTab.forEach(function(basketObj)
-//   let basketObj = basketTab[i];
 
-// }
 let url2 = " http://localhost:3000/api/products";
 let totalPriceAllProducts = 0;
 let totalAllQuantityProducts = 0;
-basketTab.forEach(function (basketObj) {
-  // Aller sur l'API chercher le reste des données de ce produit (name, price)
+basketTab.forEach(function (basketObj) {                                                // Aller sur l'API chercher le reste des données de ce produit (name, price)
+  
     apiRecup(`${url2}/${basketObj.id}`)
     .then(function (resteDonnees) {
-        let totalCurrentProduct = resteDonnees.price * basketObj.quantity;
-        totalAllQuantityProducts += basketObj.quantity   
-        console.log("totalCurrentProduct",totalCurrentProduct)           // Ajoute  la QUANTITE de ce produit au total de la quantité des produits
-        totalPriceAllProducts += totalCurrentProduct                                   // equivalent (raccourcis) ==>totalAllProducts = totalAllProducts + totalCurrentProduct Ajoute le prix total de ce produits au prix total de tout les produits 
+        let totalCurrentProduct = resteDonnees.price * basketObj.quantity;              //multiplication du prix dans reste des données x la quantitée de produits
+        totalAllQuantityProducts += basketObj.quantity                                  
+        console.log("totalCurrentProduct",totalCurrentProduct)                           // Ajoute  la QUANTITE de ce produit au total de la quantité des produits
+        totalPriceAllProducts += totalCurrentProduct                                     // equivalent (raccourcis) ==>totalAllProducts = totalAllProducts + totalCurrentProduct Ajoute le prix total de ce produits au prix total de tout les produits 
         document.getElementById(
             "cart__items"
         ).innerHTML += `<article class="cart__item" data-id="${basketObj.id}" data-color="{product-color}">
@@ -38,14 +35,14 @@ basketTab.forEach(function (basketObj) {
                 </div>
             </div>
             </article>`;
-        document.getElementById("totalQuantity").innerText = totalAllQuantityProducts;
-        document.getElementById("totalPrice").innerText = totalPriceAllProducts;
+        document.getElementById("totalQuantity").innerText = totalAllQuantityProducts;                              // on met le total de la quantité de produits dans l'id "totalAllQuantityProducts"
+        document.getElementById("totalPrice").innerText = totalPriceAllProducts;                                    // on met le total des prix des produits dans l'id "totalPriceAllProducts"
         
         const deleteItem = document.querySelectorAll(".deleteItem")
         deleteItem.forEach((btn) => {
             btn.addEventListener('click', e => {
                 localStorage.setItem("itemBasket", JSON.stringify(basketTab.filter(basketObj => basketObj.id != e.target.id)));         //ici on retire un objet 
-                window.location = window.location
+                window.location = window.location                                                                                       // ici on refraichit l'image
             })
         })
     })
@@ -55,29 +52,3 @@ basketTab.forEach(function (basketObj) {
     })
 })
 
-
-//quand je click sur "supprimer"    
-//tchek => y a t il des produits si oui alors retirer le produit sinon rien faire
-// 
-
-
-        // let clickDelete = document.querySelector("#deleteItem");                  
-        //  click.addEventListener("click", function () {                      
-        //    console.log("Le clique fonctionne pour la fonction addBasket");
-        //    });                                          
- 
-//   } 
-
-// Dans LocalStorage : suppression de l'article sélectionné //
-// function deleteItemSelect(e, items) {
-//     let index = e.target.classList[1].slice();
-//     items.splice(index, 1);
-//     localStorage.setItem('anyItem', JSON.stringify(items));
- 
-//     if (items.length === 0) {
-//         localStorage.removeItem('anyItem');
-//     }
-//     updateNumberArticles();
-// }
-
-// Btn supprimer article 
