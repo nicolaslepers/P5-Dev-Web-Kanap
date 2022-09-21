763bbdd925d2753591b919c41f91997b67061084
Nicolas Lepers <Lepers.n@gmail.com>
2022-09-21 11:27:36 +0200

    2 carts

--- front/js/cart2.js
+++ front/js/cart2.js
@@ -13,9 +13,12 @@ function afficheBasketObj(basketObj) {                                          
         totalAllQuantityProducts += basketObj.quantity                                      
         console.log("totalCurrentProduct",totalCurrentProduct)                                                                      // Ajoute  la QUANTITE de ce produit au total de la quantité des produits
         totalPriceAllProducts += totalCurrentProduct                                                                                // equivalent (raccourcis) ==> totalAllProducts = totalAllProducts + totalCurrentProduct Ajoute le prix total de ce produits au prix total de tout les produits 
-        document.getElementById(                                                                                                    // ensuite on incremente les diversesbasket.obj (id, color) dans le code html
-            "cart__items"
-        ).innerHTML += `<article class="cart__item" data-id="${basketObj.id}" data-color="{product-color}">
+        const basketObjHtml = document.createElement("article")
+        basketObjHtml.setAttribute("class", "cart__item")
+        basketObjHtml.setAttribute("data-id", basketObj.id)
+        basketObjHtml.setAttribute("data-color", basketObj.color)
+        document.getElementById("cart__items").appendChild(basketObjHtml)
+        basketObjHtml.innerHTML = `
             <div class="cart__item__img">
                 <img src="${resteDonnees.imageUrl}" alt="Photographie d'un canapé">
             </div>
@@ -31,21 +34,18 @@ function afficheBasketObj(basketObj) {                                          
                     <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${basketObj.quantity}">
                 </div>
                 <div class="cart__item__content__settings__delete">
-                    <p class="deleteItem" id="${basketObj.id}">Supprimer</p>
+                    <p class="deleteItem">Supprimer</p>
                 </div>
                 </div>
-            </div>
-            </article>`;
+            </div>`;
         document.getElementById("totalQuantity").innerText = totalAllQuantityProducts;                                                  // on met le total de la quantité de produits dans l'id "totalAllQuantityProducts"
         document.getElementById("totalPrice").innerText = totalPriceAllProducts;                                                        // on met le total des prix des produits dans l'id "totalPriceAllProducts"
-        
-        // Il recupere tout les elements qui ont pour class "deleteItem"
-        const deleteItem = document.querySelectorAll(".deleteItem")
-        deleteItem.forEach((btn) => {                                                                                                   // pour chaque bouton deleteItem on lance la fonction
-            btn.addEventListener('click', e => {
-                localStorage.setItem("itemBasket", JSON.stringify(basketTab.filter(bObj => bObj.id != basketObj.id || bObj.color != e.target.color)));         //ici on retire un objet du tableau grace à sont ID
-                location.reload();                                                                                   // ici on refraichit l'image
-            })
+
+        // Recuperer juste le deleteItem du basketObj en cours
+        const btn = basketObjHtml.querySelector(".deleteItem")
+        btn.addEventListener('click', e => {
+            localStorage.setItem("itemBasket", JSON.stringify(basketTab.filter(bObj => bObj.id != basketObj.id || bObj.color != basketObj.color)));         //ici on retire un objet du tableau grace à sont ID
+            location.reload();                                                                                   // ici on refraichit l'image
         })
     })
     .catch(function (erreur) {
@@ -54,9 +54,19 @@ function afficheBasketObj(basketObj) {                                          
     })
 }
 
-// Utilisation des Fonctions
-basketTab.forEach(basketObj => afficheBasketObj(basketObj))
 
+
+// Utilisation des Fonctions
+// 100 
+basketTab.forEach(basketObj => afficheBasketObj(basketObj))
+// Il recupere tout les elements qui ont pour class "deleteItem"
+const deleteItem = document.querySelectorAll(".deleteItem")
+deleteItem.forEach((btn) => {                                                                                                   // pour chaque bouton deleteItem on lance la fonction
+    btn.addEventListener('click', e => {
+        localStorage.setItem("itemBasket", JSON.stringify(basketTab.filter(bObj => bObj.id != basketObj.id || bObj.color != basketObj.color)));         //ici on retire un objet du tableau grace à sont ID
+        location.reload();                                                                                   // ici on refraichit l'image
+    })
+})
 
 
 // let form = document.querySelector('.cart__order__form__question')
